825d7c3530ad5f48bcdb91452f3728649ba14c23
rachelwoody <rachelemma99@gmail.com>
2019-01-04 14:25:32 -0800

    Add files via upload

---
+++ app/models/trainer.rb
@@ -0,0 +1,8 @@
+class Trainer < ApplicationRecord
+
+  has_many :pokemons
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+end
